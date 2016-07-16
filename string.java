7c03064eb237f3028460cdc7965d9ddca46319fc
kowsalya11 <kowsalya996@gmail.com>
2016-07-16 15:55:55 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,8 @@
+public static void main(String[] args) {
+        
+      Scanner sc=new Scanner(System.in);
+      String a=sc.nextLine();
+      String b=new  StringBuffer(a).reverse().toString();
+        System.out.println("reverse string"+b);
+    }
+}
