2b90b7bcb94d6be4c24e59cf6afc4306241ff932
kowsalya11 <kowsalya996@gmail.com>
2016-07-16 14:44:35 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("it is even no");
+        }else
+        {
+            System.out.println("it is not a even no");
+        }
+    }
+}
